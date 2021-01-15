a108022326c1ea4148e83ef188be947b7e05dbe7
uki0327 <38398949+uki0327@users.noreply.github.com>
2021-01-15 10:47:58 +0900

    .

---
+++ HelloKotlin/src/hello.kt
@@ -0,0 +1,13 @@
+import kotlin.math.*
+
+fun main() {
+    var name: String = "kotlin"
+    var age: Int = 10
+
+    println(name)
+    println(age)
+    println(PI)
+    println(max(10,20))
+    println(min(10,20))
+
+}
